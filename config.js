0b7189fccf884b9bc628efdb81a40a89ffa5c39a
nithish <nithish162965@gmail.com>
2024-10-09 18:35:46 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDxYvb7hkcMVI6OaJ0fOx4ThoIz0KLJXmM",
+    authDomain: "post-786c6.firebaseapp.com",
+    projectId: "post-786c6",
+    storageBucket: "post-786c6.appspot.com",
+    messagingSenderId: "112261326896",
+    appId: "1:112261326896:web:5b4245e0f2b746955c25f5",
+    measurementId: "G-F1PJ2XBWQ0"
+  };
+  
+  export { firebaseConfig };
+  
